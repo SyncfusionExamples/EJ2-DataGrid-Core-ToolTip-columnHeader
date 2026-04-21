37b8b0fd00673fd48305d902a9efb466d7845f0f
Sivaranjani Rajasekaran <sivaranjani.r@syncfusion.com>
2026-04-21 20:18:53 +0530

    Documentation(1023120): Updated sample and readme

--- README.md
+++ README.md
@@ -1,14 +1,49 @@
 # ASP.NET CORE DataGrid - ToolTip ColumnHeader
 
-This sample explains about how to show the tooltip for a particular header in the ASP.NET Core Grid.
+## Repository Description
+
+This repository contains an ASP.NET Core sample application that demonstrates the EJ2 DataGrid with remote data binding and tooltip integration for grid column headers using Syncfusion EJ2 components.
+
+## Project Overview
+
+The application renders an EJ2 DataGrid inside an EJ2 Tooltip component to display contextual information for grid headers. A tooltip is dynamically updated during mouse interactions and displays the header text when the pointer moves over specific columns. The sample focuses on combining grid rendering, remote data binding, and client-side event handling within an ASP.NET Core MVC environment.
+
+## Features
+
+- EJ2 DataGrid integration with ASP.NET Core 
+- Remote data binding using server-provided data
+- Tooltip rendering for grid header columns
+- Dynamic tooltip content update on mouse interaction
+- Clean Razor and JavaScript integration
 
 ## Prerequisites
 
-Visual Studio 2022
+- .NET Core SDK 8.x
+- Visual Studio 2019 or later
+- ASP.NET Core MVC knowledge
+- Syncfusion EJ2 packages referenced in the project
 
-## How to run this application?
+## Running the application
 
-* Checkout this project to a location in your disk.
-* Open the solution file using the Visual Studio 2022.
-* Restore the NuGet packages by rebuilding the solution.
-* Run the project.
+1. Clone the repository to the local machine.
+    ```
+    git clone <repo_link>
+    ```
+2. Open the solution file in Visual Studio.
+3. Restore all NuGet packages.
+4. Run the application using the following commands:
+    ```
+    dotnet build
+    dotnet run
+    ```
+
+## Usage
+
+When the application loads, the grid retrieves data from the server and renders it. Moving the mouse over configured header columns displays a tooltip showing the corresponding header text.
+
+### Reference
+
+- [Getting started with ASP.NET CORE Grid](https://ej2.syncfusion.com/aspnetcore/documentation/grid/getting-started-core) 
+- [Databinding](https://ej2.syncfusion.com/aspnetcore/documentation/grid/data-binding/data-binding)
+- [Tooltip](https://ej2.syncfusion.com/aspnetcore/documentation/tooltip/getting-started)
+- [API Reference](https://help.syncfusion.com/cr/aspnetcore-js2/syncfusion.ej2.grids.grid.html) 
--- WebApplication7/Startup.cs
+++ WebApplication7/Startup.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace WebApplication7
 {
@@ -21,26 +15,14 @@ namespace WebApplication7
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // Register services
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-            services.AddMvc().AddJsonOptions(options =>
-            {
-                options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver();
-            });
-
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        // Configure HTTP request pipeline
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -52,15 +34,15 @@ namespace WebApplication7
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
 
-            app.UseMvc(routes =>
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
